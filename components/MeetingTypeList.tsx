b377a2005eef369f9a25fa1e822a1ebaa38f7a4e
kshitizvardhan <kshitizvardhan8@gmail.com>
2025-01-03 23:51:44 +0530

    Update MeetingTypeList component

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,37 +1,43 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import HomeCard from "./HomeCard";
+import { useRouter } from "next/navigation";
+
 
 const MeetingTypeList = () => {
+
+  const router = useRouter();
+  const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>(undefined);
+  
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
-        img="/icons/add-meeting.svg"
+        img="/icons/add-personal.svg"
         title="New Meeting"
         description="Start an instant meeting"
         backgroundColor="bg-orange-1"
-        handleClick={() => {}}
+        handleClick={() => {setMeetingState('isInstantMeeting')}}
       />
       <HomeCard
         img="/icons/join-meeting.svg"
         title="Join Meeting"
         description="via invitation link"
         backgroundColor="bg-blue-2"
-        handleClick={() => {}}
+        handleClick={() => {setMeetingState('isJoiningMeeting')}}
       />
       <HomeCard
         img="/icons/schedule.svg"
         title="Schedule Meeting"
         description="Plan your meeting"
         backgroundColor=""
-        handleClick={() => {}}
+        handleClick={() => {setMeetingState('isScheduleMeeting')}}
       />
       <HomeCard
         img="/icons/recordings.svg"
         title="View Recordings"
         description="Meeting Recordings"
         backgroundColor="bg-yellow-1"
-        handleClick={() => {}}
+        handleClick={() => {router.push('/recordings')}}
       />
     </section>
   );
