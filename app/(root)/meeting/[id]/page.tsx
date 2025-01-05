f48a9088beb27f13bcfa94db94a44542e84f913e
kshitizvardhan <kshitizvardhan8@gmail.com>
2025-01-05 20:30:02 +0530

    Add all pages of upcoming, previous, recording and personal room

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Alert from '@/components/Alert';
+import Loader from '@/components/Loader';
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import { useGetCallById } from '@/hooks/useGetCallById';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import React, { useState } from 'react';
+
+const Meeting = ({ params }: { params: Promise<{ id: string }> }) => {
+  
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false)
+
+  // Unwrap the `params` Promise using `React.use()`  -> becoz the params are asynchronous
+  const meetingParams = React.use(params);
+  const { id } = meetingParams;
+  console.log(id);
+  const { call, isCallLoading } = useGetCallById(id);
+  
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call) return (
+    <p className="text-center text-3xl font-bold text-white">
+      Call Not Found
+    </p>
+  );
+
+  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
+
+  return (
+    <main className='h-screen w-full'>
+      <StreamCall call={call}>
+        <StreamTheme>
+            {!isSetupComplete ? (
+              <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+            ):(
+              <MeetingRoom />
+            )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
+};
+
+export default Meeting;
