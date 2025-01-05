2b3ceed2aa823a2ce7d4b2c02a210d1831a6672d
kshitizvardhan <kshitizvardhan8@gmail.com>
2025-01-05 20:29:21 +0530

    Build meeting card component

---
+++ components/MeetingCard.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { avatarImages } from "@/constants";
+import { useToast } from "@/hooks/use-toast";
+import { WobbleCard } from "./ui/wobble-card";
+
+interface MeetingCardProps {
+  title: string;
+  date: string;
+  icon: string;
+  isPreviousMeeting?: boolean;
+  buttonIcon1?: string;
+  buttonText?: string;
+  handleClick: () => void;
+  link: string;
+}
+
+const Noise = () => {
+  return (
+    <div
+      className="absolute inset-0 w-full h-full scale-[1.2] transform opacity-10 rounded-[14px] pointer-events-none"
+      style={{
+        backgroundImage: "url(/noise.webp)",
+        backgroundSize: "30%",
+      }}
+    ></div>
+  );
+};
+
+const MeetingCard = ({
+  icon,
+  title,
+  date,
+  isPreviousMeeting,
+  buttonIcon1,
+  handleClick,
+  link,
+  buttonText,
+}: MeetingCardProps) => {
+  const { toast } = useToast();
+
+  return (
+   
+    <WobbleCard className="relative flex min-h-[258px] w-full flex-col justify-between rounded-[14px] px-5 py-8 xl:max-w-[568px] overflow-hidden">
+    
+      <article className="flex flex-col gap-5 relative z-10">
+        <Image src={icon} alt="upcoming" width={28} height={28} />
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-2xl font-bold">{title}</h1>
+            <p className="text-base font-normal">{date}</p>
+          </div>
+        </div>
+      </article>
+
+      <article className={cn("flex justify-center relative z-10", {})}>
+        <div className="relative flex w-full max-sm:hidden">
+          {avatarImages.map((img, index) => (
+            <Image
+              key={index}
+              src={img}
+              alt="attendees"
+              width={40}
+              height={40}
+              className={cn("rounded-full", { absolute: index > 0 })}
+              style={{ top: 0, left: index * 28 }}
+            />
+          ))}
+          <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
+            +5
+          </div>
+        </div>
+
+        {!isPreviousMeeting && (
+          <div className="flex gap-2">
+            <Button onClick={handleClick} className="rounded bg-blue-2 px-6">
+              {buttonIcon1 && (
+                <Image src={buttonIcon1} alt="feature" width={20} height={20} />
+              )}
+              &nbsp; {buttonText}
+            </Button>
+            <Button
+              onClick={() => {
+                navigator.clipboard.writeText(link);
+                toast({
+                  title: "Link Copied",
+                });
+              }}
+              className="bg-dark-4 px-6"
+            >
+              <Image
+                src="/icons/copy.svg"
+                alt="feature"
+                width={20}
+                height={20}
+              />
+              &nbsp; Copy Link
+            </Button>
+          </div>
+        )}
+      </article>
+    </WobbleCard>
+  );
+};
+
+export default MeetingCard;
