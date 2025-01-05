a23c5b8e7415640a9cdc828766537da0e83b5abd
kshitizvardhan <kshitizvardhan8@gmail.com>
2025-01-05 15:46:24 +0530

    Update Loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className='flex-center h-screen w-full'>
+    <div className='flex-center h-screen w-full text-white gap-3'>
         <Image 
             src={"/icons/loading-circle.svg"} 
             alt='Loading'
             width={50}
             height={50}
         />
+        <p className='text-sm'>Just a Moment</p>
     </div>
   )
 }
